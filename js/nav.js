0e930a2a765c264b581d325cae411718b58fce2b
rhnypsr <yepesrojan@gmail.com>
2020-03-07 01:00:35 -0800

    first commit

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+const navslide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlinks');
+    const navLinks = document.querySelectorAll('.navlinks li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+            }
+        });
+        burger.classList.toggle('tog');
+    });
+}
+navslide();
